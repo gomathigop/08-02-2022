74859cf8aa51b48f5416760feaf4086549ab8216
GOMATHI KAVI <GOMATHI KAVI@DESKTOP-AB6DJ25>
2022-02-08 19:38:44 -0800

    First Maven Project Pushed into Github

---
+++ 08-02-2022/src/main/java/com/MavenProject08_02_2022/App.java
@@ -0,0 +1,13 @@
+package com.MavenProject08_02_2022;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
